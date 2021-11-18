9c61205d6fe83b4b6a45e56aa49d5263928769cb
Jeremy Blair <jblair@alpine.local>
2021-11-18 16:24:01 -0700

    Added support for activating/deactivating any object.
    Added support for activating/deactivating any object.

--- Unity/Scripts/3D/ActivateComponentWhenHit.cs
+++ Unity/Scripts/3D/ActivateComponentWhenHit.cs
@@ -8,15 +8,54 @@ Credit would be nice but is not mandatory.
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Reflection;
+using System;
 
 public class ActivateComponentWhenHit : MonoBehaviour
 {
+    public enum ActiveMode { enable,disable }
     public Component ComponentToActivateOnCollision;
+    public ActiveMode mode = ActiveMode.enable;
+    public bool AllowToggle = false;//allows the state to be reset if its already enabled/disabled.
+    private FieldInfo fieldInfo;
+    private PropertyInfo propertyInfo;
+    public double SecondsToAllowNextActivation = 1;
+    
+    DateTime lastToggleTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastToggleTime = DateTime.Now.AddSeconds(-SecondsToAllowNextActivation);
+        FieldInfo[] myFieldInfo;
+
+        Type myType = ComponentToActivateOnCollision.GetType();
+        // Get the type and fields of FieldInfoClass.
+        myFieldInfo = myType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance
+            | BindingFlags.Public);
+
+        for (int i = 0; i < myFieldInfo.Length; i++)
+        {
+            if (myFieldInfo[i].Name == "enabled")
+            {
+                fieldInfo = myFieldInfo[i];
+            }
+        }
+
+        if(fieldInfo==null)
+        {
+            PropertyInfo[] myPropertyInfo;
+            myPropertyInfo = myType.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance
+           | BindingFlags.Public);
+
+            for (int i = 0; i < myPropertyInfo.Length; i++)
+            {
+                if (myPropertyInfo[i].Name == "enabled")
+                {
+                    propertyInfo = myPropertyInfo[i];
+                }
+            }
+        }
     }
 
     // Update is called once per frame
@@ -30,16 +69,51 @@ public class ActivateComponentWhenHit : MonoBehaviour
 
     private void OnTriggerEnter(Collider collision)
     {
-        if (enabled)
+        if (enabled && (DateTime.Now - lastToggleTime).TotalSeconds > SecondsToAllowNextActivation)
         {
             if (collision.gameObject.tag.ToLower() == TagOfObjectToCauseActivation.ToLower())
             {
-                if (ComponentToActivateOnCollision is Collider)
+                if (fieldInfo != null)
                 {
-                    ((Collider)ComponentToActivateOnCollision).enabled = true;
+                    bool currentValue = (bool)fieldInfo.GetValue(ComponentToActivateOnCollision);
+                    if(mode == ActiveMode.enable)
+                    {
+                        if(!currentValue)
+                            fieldInfo.SetValue(ComponentToActivateOnCollision, true);
+                        else if(currentValue && AllowToggle)
+                            fieldInfo.SetValue(ComponentToActivateOnCollision, false);
+                    }
+                    else {
+
+                        if (currentValue)
+                            fieldInfo.SetValue(ComponentToActivateOnCollision, false);
+                        else if (!currentValue && AllowToggle)
+                            fieldInfo.SetValue(ComponentToActivateOnCollision, true);
+                    }
+                    
+                }
+                if (propertyInfo != null)
+                {
+                    bool currentValue = (bool)propertyInfo.GetValue(ComponentToActivateOnCollision);
+                    if (mode == ActiveMode.enable)
+                    {
+                        if (!currentValue)
+                            propertyInfo.SetValue(ComponentToActivateOnCollision, true);
+                        else if (currentValue && AllowToggle)
+                            propertyInfo.SetValue(ComponentToActivateOnCollision, false);
+                    }
+                    else
+                    {
+                        if (currentValue)
+                            propertyInfo.SetValue(ComponentToActivateOnCollision, false);
+                        else if (!currentValue && AllowToggle)
+                            propertyInfo.SetValue(ComponentToActivateOnCollision, true);
+                    }
+
                 }
             }
+            lastToggleTime = DateTime.Now;
         }
     }
-
 }
+
